4d60386fee7fc1a8c53dbe29ec9d54ab4a9a8529
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-09-02 15:43:28 +0100

    Update log4j uso

--- src/test/java/com/fakestoreapi/tests/functional/ProductsTest.java
+++ src/test/java/com/fakestoreapi/tests/functional/ProductsTest.java
@@ -2,6 +2,8 @@ package com.fakestoreapi.tests.functional;
 
 import com.fakestoreapi.BaseTest;
 import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +12,11 @@ import static org.hamcrest.Matchers.*;
 
 @DisplayName("Classe de testes funcionais dos recursos")
 public class ProductsTest extends BaseTest {
-
+    private static final Logger log = LogManager.getLogger(ProductsTest.class);
     @Test
     @DisplayName("Deve listar todos produtos")
     public void deveListarTodosProdutos(){
-        given()
+        String responseString = given()
             .log().all()
         .when()
             .get("/products")
@@ -23,6 +25,9 @@ public class ProductsTest extends BaseTest {
             .statusCode(HttpStatus.SC_OK)
             .body("", hasSize(20))
             .body("title", hasItem("Solid Gold Petite Micropave "))
+            .extract().body().asString()
         ;
+
+        log.info(responseString);
     }
 }
